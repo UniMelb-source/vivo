bd104029fe558950b3a56ec2d4c4890c7f49a09f
Tom <tom@onyx.(none)>
2012-10-22 16:10:10 +1100

    - Clean up code in existing two stage role creation - Add new generator for adding project to person

---
+++ src/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/generators/AddProjectRoleToPersonGenerator.java
@@ -0,0 +1,43 @@
+package edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators;
+
+import java.util.HashMap;
+
+import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
+
+public class AddProjectRoleToPersonGenerator extends AddRoleToPersonTwoStageGenerator {
+
+    private static String template = "addProjectRoleToPerson.ftl";
+
+    @Override
+    String getTemplate() {
+        return template;
+    }
+
+    @Override
+    String getRoleType() {
+        return "https://rdr.unimelb.edu.au/config/ProjectRole";
+    }
+
+    @Override
+    RoleActivityOptionTypes getRoleActivityTypeOptionsType() {
+        return RoleActivityOptionTypes.HARDCODED_LITERALS;
+    }
+
+    @Override
+    String getRoleActivityTypeObjectClassUri(VitroRequest vreq) {
+        return null;
+    }
+
+    @Override
+    protected HashMap<String, String> getRoleActivityTypeLiteralOptions() {
+        HashMap<String, String> literalOptions = new HashMap<String, String>();
+        literalOptions.put("", "Select type");
+        literalOptions.put("http://vivoweb.org/ontology/core#Project", "Project");
+        return literalOptions;
+    }
+
+    @Override
+    boolean isShowRoleLabelField() {
+        return true;
+    }
+}
