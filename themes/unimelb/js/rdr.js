fa356129324719e5889ac3b1cf3eeb9186140f6a
Judd Kirby <j.kirby@jksoftware.com.au>
2013-03-21 13:31:15 +1100

    Added jquery cookie library.  Now track current and previous page and custom form cancel link now uses that to link back to previous page and not let form handle it.  Overwrite 'content verified?' field with radio buttons.

--- themes/unimelb/js/rdr.js
+++ themes/unimelb/js/rdr.js
@@ -1,4 +1,13 @@
+'use strict';
+
 $(document).ready(function() {
+    /*
+     * Constant declarations
+     */
+    var COOKIE_NAME_PREVIOUS_PAGE = 'rdr_previous_page';
+    var COOKIE_NAME_CURRENT_PAGE = 'rdr_current_page';
+    var DEFAULT_REDIRECT_PAGE = '/';
+
     // Data Management Plan show/hide section
     $('#dataManagementPlanDescriptionWrapper').hide();
 
@@ -8,11 +17,63 @@ $(document).ready(function() {
     });
 
     // Setup tooltips
-    $('a[rel=popover]').popover(
-    	{
-    		title : 'Further Information'
-    	}
-	).click(function(event) {
+    $('a[rel=popover]').popover({
+            title : 'Further Information'
+    }).click(function(event) {
 		event.preventDefault();
 	});
+
+    /*
+     * Fix to turn Content Verified? to radio buttons
+     *
+     * TODO: this check currently works off text matching
+     * and should be moved to a class or identifier
+     * if possible
+     */
+    var heading = $('div[role=main] h2 em').text();
+
+    if (heading.indexOf('content verified') >= 0) {
+        var textField = $('form textarea[name=literal]');
+        var radioField = $(
+            '<div class="radio-set">' +
+                '<input id="literalFalse" type="radio" name="literal" value="false" /><label for="literalFalse">False</label>' +
+                '<input id="literalTrue" type="radio" name="literal" value="true" /><label for="literalTrue">True</label>' +
+            '</div>');
+
+        textField.val() === "true" ? radioField.children('#literalTrue').prop('checked', true) : radioField.children('#literalFalse').prop('checked', true);
+        textField.after(radioField).remove();
+    }
+
+    /*
+     * Map the current and previous pages,
+     * this is used for the cancel buttons
+     *
+     * TODO: This could be performed on the server and should
+     * be when possible
+     */
+    $.cookie.defaults.path = '/';
+
+    /*
+     * Attempt to handle a refresh and not overwrite the
+     * previous page if so
+     */
+    if (window.location.href !== $.cookie(COOKIE_NAME_CURRENT_PAGE)) {
+        $.cookie(COOKIE_NAME_PREVIOUS_PAGE, $.cookie(COOKIE_NAME_CURRENT_PAGE));
+        $.cookie(COOKIE_NAME_CURRENT_PAGE, window.location.href);
+    }
+
+    /*
+     * For all of our custom forms, overwrite the cancel
+     * link to redirect back to the previous page
+     */
+    var rdrCustomForm = $('form.rdrCustomForm');
+
+    if (rdrCustomForm.length > 0) {
+        rdrCustomForm.find('a.cancel').click(function(event) {
+            event.preventDefault();
+            var previousPage = $.cookie(COOKIE_NAME_PREVIOUS_PAGE);
+            var redirectPage = previousPage ? previousPage : "//" + window.location.hostname + DEFAULT_REDIRECT_PAGE;
+            window.location.href = redirectPage;
+        });
+    }
 });
