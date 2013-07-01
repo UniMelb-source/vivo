a70e3535bfdcce421ebb34dd0ff7855ea51e0ba2
Thomas Sullivan <tom@nsp-precise-prod.vm.nsp.nectar.org.au>
2013-07-01 11:22:32 +1000

    - Merged vivo-installer.sh from vivo-crdr

--- vivo-installer.sh
+++ vivo-installer.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+if [ ${#} != 1 ]
+then
+	echo "Usage: ${BASH_SOURCE[0]} <mysql-password>"
+	exit
+fi
+
 SCRIPT_PATH=$(pwd)
 
 BUILD_DIR=$(mktemp -d)
 
 MYSQL_ADMIN_USER="root"
+MYSQL_ADMIN_PASSWORD=${1}
 
 VITRO_DB="vivo"
 VITRO_DB_USERNAME="vivo"
@@ -13,34 +20,31 @@ VITRO_DB_PASSWORD="vivo"
 TOMCAT_USER="tomcat6"
 TOMCAT_GROUP="tomcat6"
 
-echo $(date +%s%N | cut -b1-13) "- starting"
+VIVO_URL="http://localhost/vivo"
 
-echo -n "MySQL Password:"
-read -s MYSQL_PASSWORD
-echo
+echo $(date +%s%N | cut -b1-13) "- starting"
 
 START_TIME=$(date +%s%N | cut -b1-13)
 echo "${START_TIME} - preparing database"
-echo "DROP DATABASE IF EXISTS ${VITRO_DB}; CREATE DATABASE IF NOT EXISTS ${VITRO_DB}; GRANT ALL PRIVILEGES ON ${VITRO_DB}.* TO ${VITRO_DB_USERNAME}@localhost IDENTIFIED BY '${VITRO_DB_PASSWORD}';" | mysql --user=${MYSQL_ADMIN_USER} --password=${MYSQL_PASSWORD}
+echo "DROP DATABASE IF EXISTS ${VITRO_DB}; CREATE DATABASE IF NOT EXISTS ${VITRO_DB}; GRANT ALL PRIVILEGES ON ${VITRO_DB}.* TO ${VITRO_DB_USERNAME}@localhost IDENTIFIED BY '${VITRO_DB_PASSWORD}';" | mysql --user=${MYSQL_ADMIN_USER} --password=${MYSQL_ADMIN_PASSWORD}
 
-#VIVO_GIT="git://github.com/UniMelb-source/vivo.git"
-VIVO_GIT="/home/tom/development/vivo"
+VIVO_GIT="git://github.com/UniMelb-source/vivo.git"
 VIVO_GIT_BRANCH="vivo-crdr"
 VIVO_DIR=${BUILD_DIR}/vivo
 
-echo $(date +%s%N | cut -b1-13) "- cloning VIVO"
+NETWORK_START_TIME=$(date +%s%N | cut -b1-13)
+echo "${NETWORK_START_TIME} - cloning VIVO"
 git clone ${VIVO_GIT} ${VIVO_DIR} 1>${SCRIPT_PATH}/vivo-installer.log 2>${SCRIPT_PATH}/vivo-installer.err
 pushd ${VIVO_DIR} 1>>${SCRIPT_PATH}/vivo-installer.log 2>>${SCRIPT_PATH}vivo-installer.err
 git checkout ${VIVO_GIT_BRANCH} 1>>${SCRIPT_PATH}/vivo-installer.log 2>>${SCRIPT_PATH}/vivo-installer.err
 
 echo $(date +%s%N | cut -b1-13) "- fetching VITRO"
-#VITRO_TARBALL="https://github.com/downloads/vivo-project/Vitro/vitro-rel-1.4.1.tar.gz"
-VITRO_TARBALL="file:///home/tom/development/vitro-rel-1.4.1.tar.gz"
+VITRO_TARBALL="https://github.com/downloads/vivo-project/Vitro/vitro-rel-1.4.1.tar.gz"
 VITRO_DIR=${BUILD_DIR}/vitro
 
 mkdir -p ${VITRO_DIR}
 pushd ${VITRO_DIR} 1>>${SCRIPT_PATH}/vivo-installer.log 2>>${SCRIPT_PATH}/vivo-installer.err
-curl ${VITRO_TARBALL} 2>>${SCRIPT_PATH}/vivo-installer.err | tar --strip-components=1 -xzf -
+curl -L ${VITRO_TARBALL} 2>>${SCRIPT_PATH}/vivo-installer.err | tar --strip-components=1 -xzf -
 
 # Prepare VIVO properties
 ADMIN_EMAIL="tsullivan@unimelb.edu.au"
@@ -49,7 +53,8 @@ TOMCAT_HOME="/var/lib/tomcat6"
 VITRO_HOME="/usr/local/vivo/data"
 VIVO_LOG_DIR="/usr/share/tomcat6/logs"
 
-echo $(date +%s%N | cut -b1-13) "- preparing settings"
+NETWORK_END_TIME=$(date +%s%N | cut -b1-13)
+echo "${NETWORK_END_TIME} - preparing settings"
 cp ${VIVO_DIR}/example.deploy.properties ${VIVO_DIR}/deploy.properties
 sed -i -e "s#^vitro.core.dir = .*\$#vitro.core.dir = ${VITRO_DIR}#g" ${VIVO_DIR}/deploy.properties
 sed -i -e "s#^tomcat.home = .*\$#tomcat.home = ${TOMCAT_HOME}#g" ${VIVO_DIR}/deploy.properties
@@ -67,6 +72,12 @@ pushd ${VIVO_DIR} 1>>${SCRIPT_PATH}/vivo-installer.log 2>>${SCRIPT_PATH}/vivo-in
 echo $(date +%s%N | cut -b1-13) "- cleaning and deploying"
 ant clean deploy 1>>${SCRIPT_PATH}/vivo-installer.log 2>>${SCRIPT_PATH}/vivo-installer.err
 
+touch /etc/authbind/byport/{80,443}
+chown ${TOMCAT_USER} /etc/authbind/byport/{80,443}
+chmod 775 /etc/authbind/byport/{80,443}
+
+sed -i -e "s/^#AUTHBIND=no/AUTHBIND=yes/g" /etc/default/tomcat6
+
 # Post-build setup
 chown -R ${TOMCAT_USER}:${TOMCAT_GROUP} ${VITRO_HOME}
 chown -R ${TOMCAT_USER}:${TOMCAT_GROUP} ${VIVO_LOG_DIR}
@@ -77,7 +88,10 @@ echo $(date +%s%N | cut -b1-13) "- restarting Tomcat"
 dirs -c
 rm -rf ${BUILD_DIR}
 echo $(date +%s%N | cut -b1-13) "- fetching homepage"
-curl http://localhost:8080/vivo 1>>${SCRIPT_PATH}/vivo-installer.log 2>>${SCRIPT_PATH}/vivo-installer.err
+curl ${VIVO_URL} 1>>${SCRIPT_PATH}/vivo-installer.log 2>>${SCRIPT_PATH}/vivo-installer.err
 END_TIME=$(date +%s%N | cut -b1-13)
 echo "${END_TIME} - done"
 echo "$((END_TIME - START_TIME))ms elapsed"
+echo "- $((NETWORK_START_TIME - START_TIME))ms setup"
+echo "- $((NETWORK_END_TIME - NETWORK_START_TIME))ms network"
+echo "- $((END_TIME - NETWORK_END_TIME))ms build and deploy"
