2b7171df846a740f55fdda7a21ebdcc477f7eda3
Thomas Sullivan <tom@nsp-precise-prod.vm.nsp.nectar.org.au>
2013-08-24 18:31:12 +1000

    Install required Debian packages

--- vivo-installer.sh
+++ vivo-installer.sh
@@ -45,6 +45,8 @@ function deploy_ldap_login {
   VIVO_BUILD_DIR=${2}
   SCRIPT_PATH=${3}
 
+  apt-get install -y libapache2-mod-jk libapache2-mod-php5 php5-ldap php5-curl
+
   cp ${VIVO_BUILD_DIR}/ldap/ldaplogin.* ${WWW_DIR} 2>>${SCRIPT_PATH}/vivo-installer.err
 }
 
