c5dae463e5e9feba7c87831feef0119b49d1d97e
Simon Porter <sjcporter@gmail.com>
2013-09-02 14:48:42 +1000

    Create AddPublicationToAgreement.java

---
+++ src/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/generators/AddPublicationToAgreement.java
@@ -0,0 +1,47 @@
+package edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators;
+
+import java.util.List;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ *
+ * @author tom
+ */
+public class AddPublicationToResearchDataGenerator extends AddPublicationToThingGenerator {
+
+    private static final Log log = LogFactory.getLog(AddPublicationToAgreement.class);
+
+    @Override
+    protected Log getLog() {
+        return log;
+    }
+
+    @Override
+    protected String getTemplate() {
+        return "addPublicationToThing.ftl";
+    }
+
+    @Override
+    protected final List<String> getN3Required() {
+        return list(N3_PREFIX
+                + "?agreement core:supportedInformationResource ?publicationUri. \n"
+                + "?publicationUri core:informationResourceSupportedBy ?agreement. \n"
+                + "?publicationUri a core:InformationResource ;");
+    }
+
+    @Override
+    protected String getSubjectName() {
+        return "agreement";
+    }
+
+    @Override
+    protected String getPredicateName() {
+        return "predicate";
+    }
+
+    @Override
+    protected String getObjectName() {
+        return "publicationUri";
+    }
+}
